a61355d307bef8e5acc0699cb8aae866b46fe4dd
droune2001 <nfauvet@gmail.com>
2018-07-14 14:39:26 +0200

    solution for home

---
+++ vulkan_dev_home.bat
@@ -0,0 +1,18 @@
+@echo off
+
+echo Setup Visual Studio 2017 Environment Variables
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+
+echo Set VULKAN env vars
+
+REM use VK_SDK_PATH if existing
+
+set VULKAN_SDK_VERSION=1.1.77.0
+set VULKAN_SDK_DIR=C:\VulkanSDK\%VULKAN_SDK_VERSION%
+set PATH=%VULKAN_SDK_DIR%\Bin;%PATH%
+set VULKAN_PATH=%VULKAN_SDK_DIR%\Bin
+
+cd C:\home\dev\vulkan
+
+doskey DEV=devenv vc2017_home\vulkan.sln
