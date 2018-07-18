90358d9033455a262be75b80b72098b87186c75a
nfauvet <nfauvet@gmail.com>
2018-07-18 14:46:35 +0200

    utlis - en cours

---
+++ src/tuto5-12/utils.cpp
@@ -0,0 +1,60 @@
+#include "utils.h"
+
+namespace vk
+{
+    bool create_host_vertex_buffer(context *ctx, uint64_t size, buffer *dst)
+    {
+        return create_buffer(ctx, size, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, dst);
+    }
+
+    bool create_resident_host_vertex_buffer(context *ctx, uint64_t size, buffer *dst)
+    {
+        return create_buffer(ctx, size, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, dst);
+    }
+
+    bool create_buffer(context *ctx, uint64_t size_in_bytes, VkBufferUsageFlags usage, VkMemoryPropertyFlags desired_memory_flags, buffer *dst_buffer)
+    {
+        VkResult result;
+
+        VkBufferCreateInfo vertex_buffer_create_info = {};
+        vertex_buffer_create_info.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+        vertex_buffer_create_info.size = size_in_bytes;
+        vertex_buffer_create_info.usage = usage;
+        vertex_buffer_create_info.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+        result = vkCreateBuffer(ctx->device, &vertex_buffer_create_info, nullptr, &dst_buffer->buffer);
+        if (result != VK_SUCCESS)
+            return false;
+
+        VkMemoryRequirements vertex_buffer_memory_requirements = {};
+        vkGetBufferMemoryRequirements(ctx->device, dst_buffer->buffer, &vertex_buffer_memory_requirements);
+
+        VkMemoryAllocateInfo vertex_buffer_allocate_info = {};
+        vertex_buffer_allocate_info.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+        vertex_buffer_allocate_info.allocationSize = vertex_buffer_memory_requirements.size;
+
+        uint32_t vertex_memory_type_bits = vertex_buffer_memory_requirements.memoryTypeBits;
+        for (uint32_t i = 0; i < ctx->physical_device_memory_properties.memoryTypeCount; ++i)
+        {
+            VkMemoryType memory_type = ctx->physical_device_memory_properties.memoryTypes[i];
+            if (vertex_memory_type_bits & 1)
+            {
+                if ((memory_type.propertyFlags & desired_memory_flags) == desired_memory_flags) {
+                    vertex_buffer_allocate_info.memoryTypeIndex = i;
+                    break;
+                }
+            }
+            vertex_memory_type_bits = vertex_memory_type_bits >> 1;
+        }
+
+        result = vkAllocateMemory(ctx->device, &vertex_buffer_allocate_info, nullptr, &dst_buffer->memory);
+        if (result != VK_SUCCESS)
+            return false;
+
+        result = vkBindBufferMemory(ctx->device, dst_buffer->buffer, dst_buffer->memory, 0);
+        if (result != VK_SUCCESS)
+            return false;
+
+        return true;
+    }
+} // namespace vk
---
+++ src/tuto5-12/utils.h
@@ -0,0 +1,47 @@
+#ifndef _VULKAN_UTILS_2018_07_18_H_
+#define _VULKAN_UTILS_2018_07_18_H_
+
+#include "build_options.h"
+#include "platform.h"
+
+namespace vk {
+
+    struct context
+    {
+        VkInstance                       instance;
+        VkPhysicalDevice                 physical_device;
+        VkDevice                         device;
+        VkQueue                          graphics_queue;
+        VkQueue                          compute_queue;
+        VkQueue                          transfer_queue;
+        uint32_t                         graphics_family_index;
+        uint32_t                         compute_family_index;
+        uint32_t                         transfer_family_index;
+        VkPhysicalDeviceProperties       physical_device_properties;
+        VkPhysicalDeviceMemoryProperties physical_device_memory_properties;
+    };
+
+    struct buffer
+    {
+        VkBuffer buffer;
+        VkDeviceMemory memory;
+    };
+
+    /*
+    * usage:
+    *  create_buffer(my_device, sizeof(struct vertex) * 3, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT, &buffer);
+    */
+    bool create_buffer(context *ctx, uint64_t size, VkBufferUsageFlags usage, VkMemoryPropertyFlags desired_memory_flags, buffer *dst);
+    bool create_host_vertex_buffer(context *ctx, uint64_t size, buffer *dst);
+    bool create_resident_vertex_buffer(context *ctx, uint64_t size, buffer *dst);
+
+    // sub-section of a buffer
+    struct vertex_buffer
+    {
+        uint32_t offset; // offset inside the buffer
+        uint32_t size;   // size in bytes of the data in the buffer
+        buffer buffer;   // ref to the buffer in which the data is
+    };
+}
+
+#endif // !_VULKAN_UTILS_2018_07_18_H_
