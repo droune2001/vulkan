78d5dfcd6013ed603d929483d3e8af87d21112c0
droune2001 <nfauvet@gmail.com>
2018-07-19 14:13:03 +0200

    icosphere code

--- src/tuto5-12/utils.cpp
+++ src/tuto5-12/utils.cpp
@@ -1,5 +1,8 @@
 #include "utils.h"
 
+#include <map>
+#include <array>
+
 namespace vk
 {
     bool create_host_vertex_buffer(context *ctx, uint64_t size, buffer_t *dst)
@@ -57,4 +60,74 @@ namespace vk
 
         return true;
     }
+
+
+
+    //
+    // ICO SPHERE
+    //
+
+    using Lookup = std::map<std::pair<index_t, index_t>, index_t>;
+
+    index_t vertex_for_edge(
+        Lookup& lookup,
+        VertexList& vertices, 
+        index_t first, index_t second)
+    {
+        Lookup::key_type key(first, second);
+        if (key.first > key.second)
+            std::swap(key.first, key.second);
+
+        auto inserted = lookup.insert({key, vertices.size()});
+        if (inserted.second)
+        {
+            auto& edge0 = vertices[first];
+            auto& edge1 = vertices[second];
+            auto point = glm::normalize(edge0 + edge1);
+            vertices.push_back(point);
+        }
+
+        return inserted.first->second;
+    }
+
+    TriangleList subdivide(
+        VertexList& vertices,
+        TriangleList triangles)
+    {
+        Lookup lookup;
+        TriangleList result;
+
+        for (auto&& each : triangles)
+        {
+            std::array<index_t, 3> mid;
+            for (int edge = 0; edge < 3; ++edge)
+            {
+                mid[edge] = vertex_for_edge(lookup, vertices,
+                    each.vertex[edge], each.vertex[(edge + 1) % 3]);
+            }
+
+            result.push_back({each.vertex[0], mid[0], mid[2]});
+            result.push_back({each.vertex[1], mid[1], mid[0]});
+            result.push_back({each.vertex[2], mid[2], mid[1]});
+            result.push_back({mid[0], mid[1], mid[2]});
+        }
+
+        return result;
+    }
+
+    using IndexedMesh = std::pair<VertexList, TriangleList>;
+
+    IndexedMesh make_icosphere(int subdivisions)
+    {
+        VertexList vertices = icosahedron::vertices;
+        TriangleList triangles = icosahedron::triangles;
+
+        for (int i = 0; i < subdivisions; ++i)
+        {
+            triangles = subdivide(vertices, triangles);
+        }
+
+        return{vertices, triangles};
+    }
+
 } // namespace vk
--- src/tuto5-12/utils.h
+++ src/tuto5-12/utils.h
@@ -4,6 +4,10 @@
 #include "build_options.h"
 #include "platform.h"
 
+#include <glm/glm.hpp>
+
+#include <vector>
+
 namespace vk {
 
     struct context
@@ -42,6 +46,45 @@ namespace vk {
         uint32_t size;   // size in bytes of the data in the buffer
         buffer_t buffer; // ref to the buffer in which the data is
     };
+
+
+
+
+    //
+    // ICO SPHERE
+    //
+
+    using index_t = uint16_t;
+
+    struct triangle_t
+    {
+        index_t vertex[3];
+    };
+
+    using TriangleList = std::vector<triangle_t>;
+    using VertexList = std::vector<glm::vec3>;
+
+    namespace icosahedron
+    {
+        const float X = .525731112119133606f;
+        const float Z = .850650808352039932f;
+        const float N = 0.f;
+
+        static const VertexList vertices =
+        {
+            {-X,N,Z},{X,N,Z},{-X,N,-Z},{X,N,-Z},
+            {N,Z,X},{N,Z,-X},{N,-Z,X},{N,-Z,-X},
+            {Z,X,N},{-Z,X, N},{Z,-X,N},{-Z,-X, N}
+        };
+
+        static const TriangleList triangles =
+        {
+            {0,4,1},{0,9,4},{9,5,4},{4,5,8},{4,8,1},
+            {8,10,1},{8,3,10},{5,3,8},{5,2,3},{2,7,3},
+            {7,10,3},{7,6,10},{7,11,6},{11,0,6},{0,1,6},
+            {6,1,10},{9,0,11},{9,11,2},{9,2,5},{7,2,11}
+        };
+    }
 }
 
 #endif // !_VULKAN_UTILS_2018_07_18_H_
