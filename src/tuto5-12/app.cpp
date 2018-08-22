f6fd4ddb92fb346ef27c1c037fc32d611908106c
nfauvet <nfauvet@gmail.com>
2018-08-22 09:17:42 +0200

    started imgui integration

--- src/tuto5-12/app.cpp
+++ src/tuto5-12/app.cpp
@@ -9,6 +9,7 @@
 #include "utils.h"
 #include "Scene.h"
 
+#include "imgui.h"
 #include "glm_usage.h"
 
 #include <array>
@@ -41,7 +42,7 @@ bool VulkanApplication::init()
 
     Log("#  Creating Window\n");
     _w = new Window();
-    if (!_w->OpenWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "test"))
+    if (!_w->OpenWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "vulkan testbed"))
         return false;
 
     Log("#----------------------------------------\n");
@@ -55,6 +56,42 @@ bool VulkanApplication::init()
     BuildScene();
     
     _r->SetScene(_scene);
+
+
+
+    // Create a window called "My First Tool", with a menu bar.
+    bool my_tool_active = true;
+    ImGui::Begin("vulkan testbed", &my_tool_active, ImGuiWindowFlags_MenuBar);
+    if (ImGui::BeginMenuBar())
+    {
+        if (ImGui::BeginMenu("File"))
+        {
+            if (ImGui::MenuItem("Open..", "Ctrl+O")) { /* Do stuff */ }
+            if (ImGui::MenuItem("Save", "Ctrl+S")) { /* Do stuff */ }
+            if (ImGui::MenuItem("Close", "Ctrl+W")) { my_tool_active = false; }
+            ImGui::EndMenu();
+        }
+        ImGui::EndMenuBar();
+    }
+
+    // Edit a color (stored as ~4 floats)
+    float my_color[] = {1,1,1,1};
+    ImGui::ColorEdit4("Color", my_color);
+
+    // Plot some values
+    const float my_values[] = { 0.2f, 0.1f, 1.0f, 0.5f, 0.9f, 2.2f };
+    ImGui::PlotLines("Frame Times", my_values, IM_ARRAYSIZE(my_values));
+
+    // Display contents in a scrolling region
+    ImGui::TextColored(ImVec4(1, 1, 0, 1), "Important Stuff");
+    ImGui::BeginChild("Scrolling");
+    for (int n = 0; n < 50; n++)
+        ImGui::Text("%04d: Some text", n);
+    ImGui::EndChild();
+    ImGui::End();
+
+
+
 
     return true;
 }
