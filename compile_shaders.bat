83afbb4a2c0f64f51e179040a32b186803bd2f5b
droune2001 <nfauvet@gmail.com>
2018-09-06 13:54:32 +0200

    Maj solution boulot

--- compile_shaders.bat
+++ compile_shaders.bat
@@ -1,6 +1,11 @@
 @echo off
 echo "Compiling shaders"
 pushd data
+
 glslangvalidator.exe -V simple.vert -o simple.vert.spv
 glslangvalidator.exe -V simple.frag -o simple.frag.spv
+
+glslangvalidator.exe -V instancing.vert -o instancing.vert.spv
+glslangvalidator.exe -V instancing.frag -o instancing.frag.spv
+
 popd
