fc3864bdbad85e3758da313a0434d5b18e833f10
nfauvet <nfauvet@gmail.com>
2018-09-28 09:11:45 +0200

    cmake

--- vulkan_dev_laptop.bat
+++ vulkan_dev_laptop.bat
@@ -10,9 +10,14 @@ set VULKAN_SDK_DIR=C:\VulkanSDK\%VULKAN_SDK_VERSION%
 set PATH=%VULKAN_SDK_DIR%\Bin;%PATH%
 set VULKAN_PATH=%VULKAN_SDK_DIR%\Bin
 
+echo Set Tools Environment Variables
+set MY_CMAKE_PATH=C:\Users\Nicolas\home\bin\cmake-3.12.2-win64-x64\bin
+set PATH=%MY_CMAKE_PATH%;%PATH%
+
 echo Set Librairies Environment Variables
 set GLM_DIR=C:\Users\Nicolas\home\dev\glm-0.9.9.0
 
 cd C:\Users\Nicolas\home\dev\vulkan
 
 doskey DEV=devenv vc2017\vulkan.sln
+doskey cmaker=cmake -G"Visual Studio 15 2017 Win64" -Hsrc -Bbuild_test
