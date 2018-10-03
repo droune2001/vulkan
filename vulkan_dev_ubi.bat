526806cfaf2efcf9956262535d794d36f9416f75
droune2001 <nfauvet@gmail.com>
2018-10-03 15:05:27 +0200

    update cmake ubi

--- vulkan_dev_ubi.bat
+++ vulkan_dev_ubi.bat
@@ -10,6 +10,10 @@ set VULKAN_SDK_DIR=C:\VulkanSDK\%VULKAN_SDK_VERSION%
 set PATH=%VULKAN_SDK_DIR%\Bin;%PATH%
 set VULKAN_PATH=%VULKAN_SDK_DIR%\Bin
 
+echo Set Tools Environment Variables
+set MY_CMAKE_PATH=D:\dev\bin\cmake-3.12.2-win64-x64\bin
+set PATH=%MY_CMAKE_PATH%;%PATH%
+
 echo Set Librairies Environment Variables
 set GLM_DIR=D:\dev\glm-0.9.9.0
 
@@ -17,4 +21,6 @@ D:
 
 cd D:\dev\vulkan
 
-doskey DEV=devenv vc2015\vulkan.sln
+REM doskey DEV=devenv vc2015\Vulkan.sln
+doskey dev=devenv build\vulkan.sln
+doskey cmaker=cmake -G"Visual Studio 14 2015 Win64" -Hsrc -Bbuild
